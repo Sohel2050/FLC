1383364d10c8f95af39d45dac332168a9a258515
Dexterfury <raphaelsquared@gmail.com>
2025-08-22 18:29:06 +0200

    updates

---
+++ lib/env.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// generated_from: .env
+final class _Env {
+  static const int zegoAppId = 1156412597;
+
+  static const String zegoAppSign =
+      '831942d38a43a5bc654f1d9cc8525043d4f41a2034517b82fc38b3cd230281d2';
+}
